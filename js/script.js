395c8a7c73e2c1666a524babef2c75b8a1446ef8
Sebastian <69328030+Gato070@users.noreply.github.com>
2024-05-29 19:21:59 -0300

    Septimo commit carga Starships API

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ const cargarApi = async () => {
 }
 
 /*CONSUMIR API*/
-/* Evento al hacer click consulta api people*/
+/* Evento al hacer click consulta api vehiculos*/
 document.getElementById('misVehiculos').addEventListener('click', function (event) {
     event.preventDefault(); //Para evitar la accion del enlace
     cargarVehiculos();
@@ -126,6 +126,72 @@ const cargarVehiculos = async () => {
             });
                    
             document.getElementById('api').innerHTML = vehiculos;            
+
+        } else if (res.status === 401) {
+            console.log('La clave api es incorrecta');
+        } else if (res.status === 404) {
+            console.log('El personaje no existe');
+        } else {
+            console.log('Revisar clave algo malo paso')
+        }                 
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
+/*CONSUMIR API*/
+/* Evento al hacer click consulta api starships*/
+document.getElementById('misstarships').addEventListener('click', function (event) {
+    event.preventDefault(); //Para evitar la accion del enlace
+    cargarStarships();
+})
+
+/* Cargar vehiculos */
+
+const cargarStarships = async () => {
+    
+    try {
+
+        const res = await fetch ('https://swapi.info/api/starships');
+
+        console.log(res);
+
+        // Ver si es correcto la consulta o respuesta
+        if (res.status === 200) {
+            const datosStarships = await res.json();/* Uso await para esperar a cargar todos los datos*/
+
+            let starships = '';/* Aca se almacenan los vehiculos*/
+            
+            datosStarships.forEach((element, index) => {
+                /* Operador ternario para cambiar color si es par */
+                const borderClass = (index % 2 === 0) ? 'border-light' : 'border-success';
+                starships += `
+                                <div class="col-8 text-center mb-3 bg-transparent"> 
+                                    <div class="card bg-transparent text-light ${borderClass} m-2 p-2">
+                                        <div class="card-header ${borderClass}">${element.name}</div>
+                                            <div class="card-body">
+                                            <p class="card-text">model:${element.model}</p>
+                                            <p class="card-text">manufacturer:${element.manufacturer}</p>
+                                            <p class="card-text">cost_in_credits:${element.cost_in_credits}</p>                                            
+                                            <p class="card-text">length:${element.length}</p>
+                                            <p class="card-text">max_atmosphering_speed:${element.max_atmosphering_speed}</p>
+                                            <p class="card-text">crew:${element.crew}</p>
+                                            <p class="card-text">passengers:${element.passengers}</p>
+                                            <p class="card-text">cargo_capacity:${element.cargo_capacity}</p>
+                                            <p class="card-text">consumables:${element.consumables}</p>
+                                            <p class="card-text">hyperdrive_rating:${element.hyperdrive_rating}</p>
+                                            <p class="card-text">MGLT":${element.MGLT}</p>
+                                            <p class="card-text">starship_class:${element.starship_class}</p>
+                                        </div>
+                                    </div>
+                                </div>`;
+                
+            });
+                   
+            document.getElementById('api').innerHTML = starships;            
 
         } else if (res.status === 401) {
             console.log('La clave api es incorrecta');
