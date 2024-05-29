ff21fd3f6ee96b90166aae1987fc0e1615cc85f7
Sebastian <69328030+Gato070@users.noreply.github.com>
2024-05-29 19:01:25 -0300

    Sexto commit - consume categoria vehiculos de la api

--- js/script.js
+++ js/script.js
@@ -59,7 +59,73 @@ const cargarApi = async () => {
                 
             });
          
-            document.getElementById('personajes').innerHTML = personajes;            
+            document.getElementById('api').innerHTML = personajes;            
+
+        } else if (res.status === 401) {
+            console.log('La clave api es incorrecta');
+        } else if (res.status === 404) {
+            console.log('El personaje no existe');
+        } else {
+            console.log('Revisar clave algo malo paso')
+        }                 
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+/*CONSUMIR API*/
+/* Evento al hacer click consulta api people*/
+document.getElementById('misVehiculos').addEventListener('click', function (event) {
+    event.preventDefault(); //Para evitar la accion del enlace
+    cargarVehiculos();
+})
+/* Funcion flecha debe ser asincrona para poder usar await o sea esperar
+    que se cargue bien toda la info antes de cargar la siguiente linea
+*/
+
+/* Cargar vehiculos */
+
+const cargarVehiculos = async () => {
+    
+    try {
+
+        const res = await fetch ('https://swapi.info/api/vehicles');
+
+        console.log(res);
+
+        // Ver si es correcto la consulta o respuesta
+        if (res.status === 200) {
+            const datosVehiculos = await res.json();/* Uso await para esperar a cargar todos los datos*/
+
+            let vehiculos = '';/* Aca se almacenan los vehiculos*/
+            
+            datosVehiculos.forEach((element, index) => {
+                /* Operador ternario para cambiar color si es par */
+                const borderClass = (index % 2 === 0) ? 'border-light' : 'border-success';
+                vehiculos += `
+                                <div class="col-8 text-center mb-3 bg-transparent"> 
+                                    <div class="card bg-transparent text-light ${borderClass} m-2 p-2">
+                                        <div class="card-header ${borderClass}">${element.name}</div>
+                                            <div class="card-body">
+                                            <p class="card-text">name:${element.name}</p>
+                                            <p class="card-text">model:${element.model}</p>
+                                            <p class="card-text">manufacturer:${element.manufacturer}</p>
+                                            <p class="card-text">cost_in_credits:${element.cost_in_credits}</p>
+                                            <p class="card-text">length:${element.length}</p>
+                                            <p class="card-text">crew:${element.crew}</p>
+                                            <p class="card-text">passengers:${element.passengers}</p>
+                                            <p class="card-text">cargo_capacity:${element.cargo_capacity}</p>
+                                            <p class="card-text">consumables:${element.consumables}</p>
+                                            <p class="card-text">vehicle_class:${element.vehicle_class}</p>
+                                        </div>
+                                    </div>
+                                </div>`;
+                
+            });
+                   
+            document.getElementById('api').innerHTML = vehiculos;            
 
         } else if (res.status === 401) {
             console.log('La clave api es incorrecta');
